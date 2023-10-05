901c1fcfa384aac938c21dd8543ce7d54c822fac
mdArman2507 <mdarman2507@gmail.com>
2023-10-05 14:39:16 +0530

    styled-contents & dynamic css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import MovieList from "./movieList";
+import Navbar from "./Navbar";
 
 export default function App(){
   return (
     <>
-    <h1>Movie App</h1>
+    <Navbar/>
     <MovieList/>
     </>
   )
